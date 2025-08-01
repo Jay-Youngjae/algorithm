d89ea92ad53593d7d47595c815c59df77657b87b
Youngjae Kim <101054841+Jay-Youngjae@users.noreply.github.com>
2025-08-01 13:11:52 +0900

    [level 2] Title: 타겟 넘버, Time: 367.34 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
@@ -0,0 +1,12 @@
+def solution(numbers, target):
+    def dfs(index, current_sum, numbers, target):
+        if index == len(numbers):
+            if current_sum == target:
+                return 1
+            else:
+                return 0
+        
+        return (dfs(index+1, current_sum+numbers[index], numbers, target)+dfs(index+1, current_sum-numbers[index], numbers, target))
+    
+    answer = dfs(0, 0, numbers, target)
+    return answer
