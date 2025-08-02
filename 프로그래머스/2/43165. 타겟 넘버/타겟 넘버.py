f47e01225a24ef031a56c247a159fd803a266782
Youngjae Kim <101054841+Jay-Youngjae@users.noreply.github.com>
2025-08-02 16:52:58 +0900

    [level 2] Title: 타겟 넘버, Time: 392.38 ms, Memory: 9.21 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
@@ -1,12 +1,12 @@
 def solution(numbers, target):
-    def dfs(index, current_sum, numbers, target):
-        if index == len(numbers):
-            if current_sum == target:
+    def dfs(idx, number_sum, numbers, target):
+        if idx == len(numbers): #인덱스가 재귀의 끝까지 도달했다면
+            if number_sum == target: 
                 return 1
             else:
                 return 0
+        return (dfs(idx + 1, number_sum + numbers[idx], numbers, target) + dfs(idx + 1, number_sum - numbers[idx], numbers, target))
         
-        return (dfs(index+1, current_sum+numbers[index], numbers, target)+dfs(index+1, current_sum-numbers[index], numbers, target))
     
     answer = dfs(0, 0, numbers, target)
     return answer
