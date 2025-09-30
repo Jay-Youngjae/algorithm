e3f4be81923bb33ce22803c718bc556487b11a74
Youngjae Kim <101054841+Jay-Youngjae@users.noreply.github.com>
2025-09-30 17:33:11 +0900

    [level 2] Title: 더 맵게, Time: 1952.83 ms, Memory: 50.9 MB -BaekjoonHub

---
+++ 프로그래머스/2/42626. 더 맵게/더 맵게.py
@@ -0,0 +1,14 @@
+import heapq
+def solution(scoville, K):
+    answer = 0
+    heapq.heapify(scoville)
+    while scoville[0] < K:
+        if len(scoville) < 2:
+            return -1
+        a = heapq.heappop(scoville)
+        b = heapq.heappop(scoville)
+        s = a + b * 2
+        heapq.heappush(scoville, s)
+        answer += 1
+    
+    return answer
